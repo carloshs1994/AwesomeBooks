49122f7a5b9d648c39b53ff5a5995011351f726f
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-01-04 00:49:58 +0500

    Add functions to show book and remove book

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-const title = document.querySelector('form')
+const form = document.querySelector('form');
+const list = document.querySelector('ul');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  showBook();
+
+  title.value = "";
+  author.value = "";
+});
+
+//! Add books
+function showBook() {
+  const li = document.createElement('li');
+
+  li.innerHTML = `
+  <span>${title.value}</span><br>
+  <span>${author.value}</span>
+  <button class="remove">Remove</button>
+  <hr>
+  `;
+
+  list.appendChild(li);
+
+  //! Remove books
+  const removeBtn = document.querySelectorAll('.remove');
+
+  removeBtn.forEach(btn => {
+    btn.addEventListener('click', e => {
+      e.target.parentElement.remove();
+    })
+  })
+}
