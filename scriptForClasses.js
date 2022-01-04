00ad6b5b9a3db238e0c421c5742f3ae33bdad287
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-01-05 04:08:11 +0500

    Fix linter errors

--- scriptForClasses.js
+++ scriptForClasses.js
@@ -1,49 +1,29 @@
+/* eslint no-use-before-define: 2 */  // --> ON
+
 const list = document.querySelector('ul');
 const form = document.querySelector('form');
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 class Books {
-  constructor(){
+  constructor() {
     this.bookList = [];
   }
+
   addNewBook(newBook) {
     return this.bookList.push(newBook);
   }
+
   removeBooksFromList() {
     const removeButtons = document.getElementsByClassName('remove');
-  for(let i = 0; i < removeButtons.length; i += 1){
-    let button = removeButtons[i];
-    button.addEventListener('click', function(event) {
-      let element = event.target.parentElement.firstElementChild.firstElementChild.innerText;
-      for (let i = 0; i < books.bookList.length; i++) {
-        if (element === books.bookList[i].title) {
-          books.bookList.splice(i, 1);
-        }
-      }
-      updateDomAndLocalStorage();
-    });
-  }
+    removeBook(removeButtons);
   }
 }
+
 const books = new Books();
 
-class Book{
-  constructor (title, author) {
-  this.title = title;
-  this.author = author;
-  }
+function addToLocalStorage(books) {
+  localStorage.setItem('books', JSON.stringify(books));
 }
-
-getFromLocalStorage();
-
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  const newBook = new Book(title.value, author.value);
-  title.value = '';
-  author.value = '';
-  books.addNewBook(newBook);
-  updateDomAndLocalStorage();
-});
 
 function appendBooksToList() {
   list.innerHTML = '';
@@ -63,23 +43,52 @@ function appendBooksToList() {
   });
 }
 
-function updateDomAndLocalStorage () {
+function updateDomAndLocalStorage() {
   appendBooksToList();
   localStorage.clear();
   addToLocalStorage(books);
   books.removeBooksFromList();
 }
 
-function addToLocalStorage(books) {
-  localStorage.setItem('books', JSON.stringify(books));
+function removeBook(removeButtons) {
+  for (let i = 0; i < removeButtons.length; i += 1) {
+    const button = removeButtons[i];
+    button.addEventListener('click', event => {
+      const element = event.target.parentElement.firstElementChild.firstElementChild.innerText;
+      for (let i = 0; i < books.bookList.length; i += 1) {
+        if (element === books.bookList[i].title) {
+        books.bookList.splice(i, 1);
+        }
+      }
+      updateDomAndLocalStorage();
+    });
+  }
+}
+
+class Book{
+  constructor (title, author) {
+  this.title = title;
+  this.author = author;
+  }
 }
 
 function getFromLocalStorage() {
-  if(localStorage.length!=0){
+  if (localStorage.length!=0) {
     const booksFromLocStg = JSON.parse(localStorage.getItem('books'));
     booksFromLocStg.bookList.forEach(book => {
       books.bookList.push(book);
-    })
+    });
     updateDomAndLocalStorage();
   }
 }
+
+getFromLocalStorage();
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const newBook = new Book(title.value, author.value);
+  title.value = '';
+  author.value = '';
+  books.addNewBook(newBook);
+  updateDomAndLocalStorage();
+});
