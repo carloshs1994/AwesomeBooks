8e2b322d1e9b0c935c28f97b3fbdc351a5086421
Carlos Herver <carlos.h.s.1994@gmail.com>
2022-01-05 16:35:21 -0600

    Set date and time

--- scriptForClasses.js
+++ scriptForClasses.js
@@ -141,3 +141,76 @@ nav.addEventListener('click', (event) => {
     contactTab.classList.add('red');
   }
 });
+
+setInterval(() => {
+  let today = new Date();
+  let month = '';
+  let day = '';
+  let hour = today.getHours();
+  let hourType = 'am';
+  
+  if (hour > 12) {
+    hour -= 12;
+    hourType = 'pm';
+  } else if (hour === 12) {
+    hourType = 'pm';
+  }
+  
+  
+  switch(today.getDate()) {
+    case 1:
+      day = 'st';
+      break;
+    case 2:
+      day = 'nd';
+      break;
+    case 3:
+      day = 'rd';
+      break;
+    default:
+      day = 'th';
+  }
+  
+  switch(today.getMonth()) {
+    case 0:
+      month = 'January';
+      break;
+    case 1:
+      month = 'February';
+      break;
+    case 2:
+      month = 'March';
+      break;
+    case 3:
+      month = 'April';
+      break;
+    case 4:
+      month = 'May';
+      break;
+    case 5:
+      month = 'June';
+      break;
+    case 6:
+      month = 'July';
+      break;
+    case 7:
+      month = 'August';
+      break;
+    case 8:
+      month = 'September';
+      break;
+    case 9:
+      month = 'October';
+      break;
+    case 10:
+      month = 'November';
+      break;
+    case 11:
+      month = 'December';
+      break;
+  }
+  let date = `${month} ${today.getDate()}${day} ${today.getFullYear()},`;
+  let time = hour + ":" + today.getMinutes() + ":" + today.getSeconds() + ' ' + hourType;
+  let dateTime = date+' '+time;
+  document.querySelector('.time-and-date').innerHTML = dateTime;
+}, 1000);
